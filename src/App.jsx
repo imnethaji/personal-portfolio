7b541a1d14f862276b9e4f077998befad830b5b4
Nethaji P <reachnethajip@gmail.com>
2024-02-19 17:22:17 +0530

    Home page design changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import "./App.sass";
 import { Alert } from "@mui/material";
+import { TypeAnimation } from "react-type-animation";
 
 function App() {
   return (
     <>
       <div className="container">
-        <h1>&lt;/Hi I&apos;m Nethaji&gt;</h1>
+        <h1>Hi I&apos;m Nethaji</h1>
+
+        <TypeAnimation
+          sequence={[
+            "</I'm a Developer>", // Types 'One'
+            2000, // Waits 1s
+            "</I'm a Photographer>", // Deletes 'One' and types 'Two'
+            2000, // Waits 2s
+            "</I'm a Creator>",
+            2000, // Types 'Three' without deleting 'Two'
+            () => {
+              console.log("Sequence completed");
+            },
+          ]}
+          wrapper="span"
+          cursor={true}
+          repeat={Infinity}
+          style={{
+            fontSize: "2em",
+            display: "inline-block",
+            marginBottom: "3em",
+          }}
+        />
         <Alert variant="outlined" severity="info">
           This page is under construction. We will update soon.
         </Alert>
