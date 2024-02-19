6dcc796f0451b5436e59c078059b4ac583b0670d
Nethaji P <reachnethajip@gmail.com>
2024-02-19 16:24:01 +0530

    Home page change

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import "./App.sass";
 function App() {
   return (
     <>
-      <div>
+      <div className="container">
+        <p>Thank you for visiting</p>
         <h1>&lt;/Hi I&apos;m Nethaji&gt;</h1>
+        <p>This page will get an update soon...</p>
       </div>
     </>
   );
