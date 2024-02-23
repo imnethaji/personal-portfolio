588f15c919ac55453508c6a8e8d9a344263dcb54
Nethaji P <reachnethajip@gmail.com>
2024-02-23 17:11:24 +0530

    Home page update and footer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,9 @@ function Home() {
       <div className="container">
         <div className="sub-container">
           <h1>
-            <i>Hi I&apos;m Nethaji</i>
+            <i>
+              Hi I&apos;m <span className="hero-text">Nethaji</span>
+            </i>
           </h1>
           <TypeAnimation
             sequence={[
@@ -29,21 +31,40 @@ function Home() {
               marginBottom: "1em",
             }}
           />
-          <div className="tech-stack">
-            <h3>Tech Stack</h3>
-            <div className="logo-container">
-              <img src="/tech-stack/html.svg"></img>
-              <img src="/tech-stack/css.svg"></img>
-              <img src="/tech-stack/js.svg"></img>
-              <img src="/tech-stack/vscode.svg"></img>
-              <img src="/tech-stack/react.svg"></img>
-              <img src="/tech-stack/next-js.svg"></img>
-              <img src="/tech-stack/photoshop.svg"></img>
-              <img src="/tech-stack/figma.svg"></img>
+        </div>
+      </div>
+      <div className="tech-stack">
+        <h3>Tech Stack</h3>
+        <div className="logo-container">
+          <img src="/tech-stack/html.svg"></img>
+          <img src="/tech-stack/css.svg"></img>
+          <img src="/tech-stack/js.svg"></img>
+          <img src="/tech-stack/vscode.svg"></img>
+          <img src="/tech-stack/react.svg"></img>
+          <img src="/tech-stack/next-js.svg"></img>
+          <img src="/tech-stack/photoshop.svg"></img>
+          <img src="/tech-stack/figma.svg"></img>
+        </div>
+      </div>
+      <footer>
+        <div className="footer-container">
+          <div className="border"></div>
+          <div className="social">
+            <h4>Connect with me on</h4>
+            <div className="logos">
+              <a href="https://www.instagram.com/im_nethaji/" target="_blank">
+                <img className="social-logo" src="/insta-logo.svg" alt="" />
+              </a>
+              <a href="https://www.linkedin.com/in/imnethaji/" target="_blank">
+                <img className="social-logo" src="/linked-in-logo.svg" alt="" />
+              </a>
+              <a href="https://github.com/imnethaji" target="_blank">
+                <img className="social-logo" src="/github.svg" alt="" />
+              </a>
             </div>
           </div>
         </div>
-      </div>
+      </footer>
     </>
   );
 }
