0ab89d02efc7606e6b8edbfaee7210d63d427aa6
Nethaji P <reachnethajip@gmail.com>
2024-03-20 09:41:24 +0530

    Added BackgroundBeams

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import Nav from "./components/Nav";
+import { BackgroundBeams } from "./components/ui/background-beams";
 
 function App() {
   return (
     <>
       <Nav />
+      <main className="z-50 flex flex-col h-[85vh] ml-72 justify-center">
+        <h1 className="text-white text-[6em] font-bold">
+          Hi I&apos;m <span className="text-orange-500">Nethaji!</span>
+        </h1>
+        <h1 className="text-white text-[4em] font-bold">
+          I Photograph, <br /> I Design, <br /> I Develop
+        </h1>
+      </main>
+      <BackgroundBeams />
     </>
   );
 }
