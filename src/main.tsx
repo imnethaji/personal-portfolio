7f4100d09e625151a133c4b38a3321990a30b8c1
Nethaji P <reachnethajip@gmail.com>
2024-03-20 10:20:49 +0530

    fixed minor bugs

--- src/main.tsx
+++ src/main.tsx
@@ -13,8 +13,13 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element with id 'root' not found.");
+}
