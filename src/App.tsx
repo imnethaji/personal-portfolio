f508357eccb29ac01cbff2a7cd58a70f9a48bb9c
Nethaji P <reachnethajip@gmail.com>
2024-04-02 22:29:01 +0530

    Mobile design update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
+import { useEffect, useState } from "react";
 import Nav from "./components/Nav";
 import { BackgroundBeams } from "./components/ui/background-beams";
 
 function App() {
+  const textArr = [
+    "I Photograph Weddings",
+    "I Design Websites",
+    "I Develop Web Apps",
+  ];
+
+  const [index, setIndex] = useState(0);
+  const [heroText, setHeroText] = useState(textArr[index]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % textArr.length);
+      setHeroText(textArr[index]);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [index, textArr]);
+
   return (
     <div>
       <div>
         <Nav />
-        <main className="flex flex-col h-[85vh] ml-72 justify-center">
-          <h1 className="text-white text-[6em] font-bold">
+        <main className="flex flex-col h-[85vh] ml-72 justify-center max-sm:ml-10">
+          <h1 className="text-white text-[6em] font-bold max-sm:text-[4em]">
             Hi I&apos;m <span className="text-orange-500">Nethaji!</span>
           </h1>
-          <h1 className="text-white text-[4em] font-bold">
-            I Photograph Weddings, <br /> I Design Website, <br /> I Develop Web
-            Apps
+          <h1
+            key={index}
+            className="text-white text-[4em] font-bold max-sm:text-[2em]"
+          >
+            {heroText}
           </h1>
         </main>
       </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 const cssClasses = "text-white hover:text-orange-400 hover:cursor-pointer";
 const Nav = () => {
   return (
-    <nav className="flex justify-between items-center py-5 px-40">
-      <Link to="/">
+    <nav className="flex justify-between items-center py-5 px-40 max-sm:px-0">
+      <Link to="/" className="max-sm:m-auto">
         <img
           className="w-10"
           src="/nethaji-logo.svg"
-          alt=""
+          alt="website logo"
           id="nethaji-logo"
         />
       </Link>
-      <ul className="flex space-x-4">
+      <ul className="flex space-x-4 max-sm:hidden">
         <li className={cssClasses}>
           <Link to="/">Home</Link>
         </li>
